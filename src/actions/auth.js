e05c448d6fcd2d0c3cd60776c6199a6fb5fddfef
Kingluddite <howley.phil@gmail.com>
2018-02-11 20:52:31 -0800

    Initialize repo

---
+++ src/actions/auth.js
@@ -0,0 +1,21 @@
+import { firebase, googleAuthProvider } from '../firebase/firebase';
+
+export const login = uid => ({
+  type: 'LOGIN',
+  uid,
+});
+
+export const startLogin = () => () =>
+  firebase.auth().signInWithPopup(googleAuthProvider);
+
+// export const startLogout = () => {
+//   return() => {
+//     return firebase.auth().signOut();
+//   }
+// }
+
+export const logout = () => ({
+  type: 'LOGOUT',
+});
+
+export const startLogout = () => () => firebase.auth().signOut();
